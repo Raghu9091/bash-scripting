9b0b34fd18eaa5c9ec1cece317d0cbe7e943fece
SanjuLaptop <raghu.b.7676@gmail.com>
2025-12-03 13:33:03 +0530

    Shellscripting

--- 04_variable_arg.sh
+++ 04_variable_arg.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:: Hi Satish, How are you?"
-echo "$PERSON2:: Hello Raghu, I am fine. How about you?"
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How about you?"
 echo "$PERSON1:: What are you learning recently?"
 echo "$PERSON2:: I am Learning Shell Scripting"
