b49a5c1072e7030ac3ee38b3c8b27ab54d6182c1
SanjuLaptop <raghu.b.7676@gmail.com>
2025-12-03 13:23:05 +0530

    Shellscripting

---
+++ 02_conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# echo "Hello world"
+
+echo "Raghu:: Hi Satish, How are you?"
+echo "Satish:: Hello Raghu, I am fine. How about you?"
+echo "Raghu:: What are you learning recently?"
+echo "Satish:: I am Learning Shell Scripting"
+
